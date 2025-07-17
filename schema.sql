e9965378efc92e964a98d1421c160346a258c9fc
uraibam <108549524+uraibam@users.noreply.github.com>
2025-07-17 15:23:45 +0500

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,1 +1,26 @@
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    email TEXT UNIQUE NOT NULL,
+    traits TEXT,
+    onboarded_date TEXT
+);
+
+CREATE TABLE IF NOT EXISTS tasks (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_email TEXT,
+    task_date TEXT,
+    task TEXT,
+    is_done INTEGER DEFAULT 0
+);
+
+CREATE TABLE IF NOT EXISTS reflections (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_email TEXT,
+    reflection_date TEXT,
+    soul TEXT,
+    mind TEXT,
+    body TEXT,
+    trust TEXT,
+    value TEXT
+);
 
